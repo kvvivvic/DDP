ad89a7d4b1eb2db6ad6ac668024d8ef5403a7ce5
kvvivvic <rnjs9337@gmail.com>
2022-12-13 16:32:56 +0900

    메뉴버튼 애니메이션

--- js/main.js
+++ js/main.js
@@ -3,7 +3,11 @@ $(document).ready(function () {
   $(".programs>li").hide();
   $(".programs>li").slice(0, 10).show();
 });
-
 function more() {
   $(".programs>li").slice(0, 20).show();
 }
+
+//메뉴버튼 애니메이션
+$(".menu").click(function () {
+  $(this).toggleClass("active");
+});
