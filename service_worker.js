66ee2de59ad4ba5f6021f0807d4a66e8619afe09
kvvivvic <rnjs9337@gmail.com>
2022-12-30 12:54:55 +0900

    header , footer include 형식으로 변경

--- service_worker.js
+++ service_worker.js
@@ -31,10 +31,10 @@ self.addEventListener("fetch", (pEvent) => {
       .match(pEvent.request)
       .then((response) => {
         if (!response) {
-          console.log("네트워크로 데이터 요청!", pEvent.request);
+          // console.log("네트워크로 데이터 요청!", pEvent.request);
           return fetch(pEvent.request);
         }
-        console.log("캐시에서 데이터 요청!", pEvent.request);
+        // console.log("캐시에서 데이터 요청!", pEvent.request);
         return response;
       })
       .catch((err) => console.log(err))
