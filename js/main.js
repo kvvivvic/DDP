333c49befcf3a2997efd626618e5cd969a253f89
kvvivvic <rnjs9337@gmail.com>
2023-01-03 14:33:24 +0900

    경로

--- js/main.js
+++ js/main.js
@@ -23,14 +23,13 @@ $(".menu").click(function () {
   }
 });
 
-//3depth 메뉴
 // html dom 이 다 로딩된 후 실행된다.
 $(document).ready(function () {
   // menu 클래스 바로 하위에 있는 a 태그를 클릭했을때
   $(".depth3_btn > span").click(function () {
     var submenu = $(this).next("ul");
 
-    // submenu 가 화면상에 보일때는 위로 보드랍게 접고 아니면 아래로 보드랍게 펼치기
+    // submenu 가 화면상에 보일때는 위로 부드럽게 접고 아니면 아래로 부드럽게 펼치기
     if (submenu.is(":visible")) {
       submenu.slideUp();
     } else {
